fb936e92d5cc09e585605fd99a6688e82dfa1b56
valde <mr.valdemare@gmail.com>
2026-08-10 12:09:36 +0300

    Добавлен gateway-service с настройками маршрутизации микросервисов

---
+++ gateway-service/src/test/java/com/quickbite/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickbite.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
